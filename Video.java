8b01fc3d29a5eb8cbddeecbb8757f7b5f548ee57
pratiksg <48197311+pratiksg@users.noreply.github.com>
2019-03-05 16:36:39 +0530

    name updated

--- Video.java
+++ Video.java
@@ -1,7 +1,9 @@
 class video
 {
     public static void main(string []args)
-{
-    System.out.println("hello world");
-}
+    {
+        System.out.println("hello world");
+        System.out.println("hello kapil");
+        System.out.println("hello divprapoo");
+    }
 }
